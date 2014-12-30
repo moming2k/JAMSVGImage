fb2b50443c7aa9dcbe56629c16e9ef50befc0c80
Chris Chan <chrischan@igpsd.com>
2014-12-30 20:35:15 +0800

    add JAMStyledText draft class

---
+++ Classes/JAMSVGImage/Path and Parser/JAMStyledText.h
@@ -0,0 +1,13 @@
+//
+//  JAMStyledText.h
+//  JAMSVGImage
+//
+//  Created by Chan Chris on 30/12/14.
+//  Copyright (c) 2014 Jeff Menter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JAMStyledText : NSObject
+
+@end
---
+++ Classes/JAMSVGImage/Path and Parser/JAMStyledText.m
@@ -0,0 +1,13 @@
+//
+//  JAMStyledText.m
+//  JAMSVGImage
+//
+//  Created by Chan Chris on 30/12/14.
+//  Copyright (c) 2014 Jeff Menter. All rights reserved.
+//
+
+#import "JAMStyledText.h"
+
+@implementation JAMStyledText
+
+@end
