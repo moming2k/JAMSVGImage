e60d06ab3a9dd0ceef2438c7054b7a96c8eaa283
Chris Chan <chrischan@igpsd.com>
2015-01-02 16:54:48 +0800

    prepare the scrollview for the zoom and panning

--- JAMSVGImage/JAMViewController.h
+++ JAMSVGImage/JAMViewController.h
@@ -2,10 +2,12 @@
 #import <UIKit/UIKit.h>
 #import "JAMSVGImageView.h"
 #import "JAMSVGButton.h"
+#import "TapDetectingImageView.h"
 
-@interface JAMViewController : UIViewController
+@interface JAMViewController : UIViewController <TapDetectingImageViewDelegate>
 
-@property (weak, nonatomic) IBOutlet JAMSVGImageView *svgImageView;
+@property (weak, nonatomic) IBOutlet TapDetectingImageView *svgImageView;
 @property (weak, nonatomic) IBOutlet JAMSVGButton *button;
+@property (weak, nonatomic) IBOutlet UIScrollView *imageScrollView;
 
 @end
--- JAMSVGImage/JAMViewController.m
+++ JAMSVGImage/JAMViewController.m
@@ -2,6 +2,9 @@
 #import "JAMViewController.h"
 #import "JAMSVGUtilities.h"
 
+#define ZOOM_VIEW_TAG 100
+#define ZOOM_STEP 1.5
+
 @interface UITouch (Utilities)
 
 - (CGPoint)previousTouchDeltaInView:(UIView *)view;
@@ -19,6 +22,40 @@
 @end
 
 @implementation JAMViewController
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    self.svgImageView.delegate = self;
+    [self.svgImageView setTag:ZOOM_VIEW_TAG];
+    
+    CGSize size = [self.svgImageView.svgImage size];
+    CGSize finalizeSize = [self fitSVGSizeToImageView:size];
+    
+    self.svgImageView.frame = CGRectMake(self.svgImageView.frame.origin.x, self.svgImageView.frame.origin.y, finalizeSize.width, finalizeSize.height);
+    
+    [self.imageScrollView setContentSize: finalizeSize];
+    
+
+    
+}
+
+- (CGSize)fitSVGSizeToImageView:(CGSize)imageSize //withImageViewSize:(CGSize)imageViewSize
+{
+    float ratio = 1.0f * imageSize.width / imageSize.height;
+    
+    CGSize finalSize = CGSizeMake(900 , imageSize.height );
+    return finalSize;
+}
+
+- (CGSize)fitSVGSizeToImageView2:(CGSize)imageSize withImageViewSize:(CGSize)imageViewSize
+{
+    float ratio = 1.0f * imageSize.width / imageSize.height;
+    
+    CGSize finalSize = CGSizeMake(imageViewSize.height * ratio , imageViewSize.height );
+    return finalSize;
+}
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event;
 {
